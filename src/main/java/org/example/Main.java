a6aeb027d286fbf002615b8107de1de2048de496
ArturoChoucair <jarturot@choucairtesting.com>
2024-06-11 16:06:23 -0500

    Inicio prueba SAP

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import com.tcs.certificacion.sapgui.*;
+import com.tcs.certificacion.sapgui.interactions.Click;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+    }
+}
